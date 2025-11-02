e95b7df490709c34cf77b0352f4f3e0e2830c5b6
Nasccped <pedro.main.tec@gmail.com>
2025-11-02 12:14:50 -0300

    chore: start core-dark theme

---
+++ lua/rustheme/palette/core-dark.lua
@@ -0,0 +1,26 @@
+local CoreDark = {
+    bold = true,
+    italic = true,
+    undercurl = true,
+
+    editor = {
+        foreground = "#b5c1bc",
+        weak_foreground = "#969fb1",
+        background = "#090c17",
+        weak_background = "#020508",
+        strong_background = "#4b6081",
+        contrast_color = "#151d36",
+        strong_contrast_color = "#d8795a",
+        red = "#ca3939",
+        green = "#40ca39",
+        yellow = "#e1af2d",
+        weak_red = "#491a22",
+        weak_green = "#51421e",
+        weak_yellow = "#194421",
+        cyan = "#78e3ea",
+        magenta = "#e783e1"
+    },
+    syntax = {}
+}
+
+return CoreDark
