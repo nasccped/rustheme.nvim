5335d2715433c09047f06b720464c169c98dc032
Nasccped <pedro.main.tec@gmail.com>
2025-11-04 18:37:51 -0300

    chore: set a default config

---
+++ lua/rustheme/config.lua
@@ -0,0 +1,29 @@
+local M = {}
+
+local default_config = {
+    globals = {
+        bolds = true,
+        italics = true,
+        transparents = {
+            background = false,
+            float_background = false
+        },
+        invert_title = false
+    },
+    mapping_overrides = {
+        everything = {},
+        ["core-dark"] = {},
+        ["core-light"] = {},
+        ["mangrove-dark"] = {},
+        ["mangrove-light"] = {}
+    },
+    groups_override = {}
+}
+
+--- Returns the default rustheme configuration.
+---@return config.RusthemeConfig
+function M.default()
+    return vim.deepcopy(default_config)
+end
+
+return M
