c53d92ed7c3895dfff0736f3385c54c5a4a6bf5a
Nasccped <pedro.main.tec@gmail.com>
2025-11-07 22:54:50 -0300

    chore: update plugin call

--- lua/rustheme/init.lua
+++ lua/rustheme/init.lua
@@ -1,37 +1,52 @@
-local utils = require("rustheme.lib.utils")
-local Rustheme = {}
+local conf = require("rustheme.config")
+local vimenv = require("rustheme.lib.vimenv")
+local vari = require("rustheme.lib.variant")
+local palette = require("rustheme.palettes")
+local groups = require("rustheme.groups")
+local M = { config = conf.default() }
 
----Default rustheme variant when not specified.
-local default_variant = "core-dark"
-
----Current vim background (when variant not specified).
-local current_background = utils.get_background()
-
----Local map for variant background detection.
-local background_map = {
-    ["core-dark"] = "dark",
-    ["core-light"] = "light",
-    ["mangrove-dark"] = "dark",
-    ["mangrove-light"] = "light",
-}
-
----Loads the colorscheme based on an optional theme variant.
----@param variant? string
-function Rustheme.load(variant)
-    if utils.get_colors_name() then
-        utils.run_cmd("hi clear")
+--- Loads the colorscheme based on an optional theme variant.
+---@param variant? config.AnyVariant
+function M.load(variant)
+    if vimenv.get_colors_name() then
+        vimenv.cmd("hi clear")
     end
-    if vim.fn.exists("syntax_on") then
-        utils.run_cmd("syntax reset")
+    if vimenv.fn_exists("syntax_on") then
+        vimenv.cmd("syntax reset")
     end
-    variant = variant or default_variant
-    local background = background_map[variant] or current_background
-    local colors_name = string.format("rustheme-%s", variant)
-    if background ~= utils.get_background() then
-        utils.set_background(background)
-    end
-    utils.set_colors_name(colors_name)
-    utils.set_termguicolors(true)
+    vari.setup(variant, M.config.default_variant, M.config.bg)
+    local bg = vari.get_bg()
+    variant = vari.get_variant()
+    local colors_name = string.format(
+        "rustheme %s",
+        string.gsub(variant, "-", " ")
+    )
+    vimenv.set_background(bg)
+    vimenv.set_colors_name(colors_name)
+    local cfg = vim.deepcopy(M.config.variant_config[variant])
+    cfg = vim.tbl_deep_extend("force", cfg or {}, M.config.globals)
+    local plt = vim.tbl_deep_extend(
+        "force",
+        palette.from_variant(variant),
+        M.config.palette_override or {}
+    )
+    vimenv.set_termguicolors(true)
+    local grps = vim.tbl_deep_extend(
+        "force",
+        groups.get_groups(plt, cfg or {}),
+        M.config.groups_override or {}
+    )
+    vimenv.set_hl(grps)
 end
 
-return Rustheme
+--- Set a new config table to be used by the theme.
+---@param cfg config.RusthemeConfig
+function M.setup(cfg)
+    M.config = vim.tbl_deep_extend(
+        "force",
+        M.config,
+        cfg or {}
+    )
+end
+
+return M
