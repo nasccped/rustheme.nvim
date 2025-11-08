d7b3d472a0deb316d381b27b1543e1286ab5de5d
Nasccped <pedro.main.tec@gmail.com>
2025-11-07 22:54:34 -0300

    chore: add overriding (groups/palette/cfg)

--- lua/rustheme/config.lua
+++ lua/rustheme/config.lua
@@ -1,38 +1,49 @@
 local vimenv = require("rustheme.lib.vimenv")
 local M = {}
 
+---@type config.GlobalFields
+local globals = {
+    bolds = {
+        title = true,
+        keyword = true,
+        comment = false,
+        tab_line_sel = true,
+        separators = true,
+        match_paren = true,
+        substitute = true,
+        pmenu_selection = true
+    },
+    italics = {
+        title = false,
+        keyword = true,
+        comment = true,
+        tab_line_sel = true,
+        separators = false,
+        match_paren = false,
+        substitute = false,
+        pmenu_selection = false
+    },
+    transparents = {
+        background = false,
+        float_background = false
+    }
+}
+
 ---@type config.RusthemeConfig
 local default_config = {
     default_variant = {
         dark = "core-dark",
         light = "core-light",
     },
-    globals = {
-        bolds = {
-            title = true,
-            keyword = true,
-            comment = false,
-            tab_line_sel = true,
-            separators = true,
-            match_paren = true,
-            substitute = true,
-            pmenu_selection = true
-        },
-        italics = {
-            title = false,
-            keyword = true,
-            comment = true,
-            tab_line_sel = true,
-            separators = false,
-            match_paren = false,
-            substitute = false,
-            pmenu_selection = false
-        },
-        transparents = {
-            background = false,
-            float_background = false
-        }
+    globals = vim.deepcopy(globals),
+    variant_config = {
+        ["core-dark"] = {},
+        ["core-light"] = {},
+        ["mangrove-dark"] = {},
+        ["mangrove-light"] = {}
     },
+    palette_override = {},
+    groups_override = {},
     bg = vimenv.get_background()
 }
 
@@ -40,6 +51,12 @@ local default_config = {
 ---@return config.RusthemeConfig
 function M.default()
     return vim.deepcopy(default_config)
+end
+
+--- Returns the default `config.GlobalFields`.
+---@return config.RusthemeConfig
+function M.default_global_fields()
+    return vim.deepcopy(globals)
 end
 
 return M
--- lua/rustheme/types/config.lua
+++ lua/rustheme/types/config.lua
@@ -1,11 +1,20 @@
 ---@class config.RusthemeConfig
 ---@field default_variant? config.DefaultVariant
 ---@field globals? config.GlobalFields
+---@field variant_config? config.VariantConfig
+---@field palette_override? table Color palette to extend/override the original one.
+---@field groups_override? table Group mapping to override the generated one.
 ---@field bg? "light"|"dark" The current background being used.
 
 ---@class config.DefaultVariant Maps the rustheme variant based on a background mode.
 ---@field light? config.LightVariant Default variant for light background.
 ---@field dark? config.DarkVariant Default variant for dark background.
+
+---@class config.VariantConfig Same as `config.GlobalFields`, but applied only to a specific variant.
+---@field ["core-dark"]? config.GlobalFields Global fields applied to "core-dark" variant.
+---@field ["core-light"]? config.GlobalFields Global fields applied to "core-light" variant.
+---@field ["mangrove-dark"]? config.GlobalFields Global fields applied to "mangrove-dark" variant.
+---@field ["mangrove-light"]? config.GlobalFields Global fields applied to "mangrove-light" variant.
 
 ---@alias config.AnyVariant config.LightVariant|config.DarkVariant Alias for variant option.
 ---@alias config.LightVariant "core-light"|"mangrove-light" Alias for light variants.
