f5fe3b7986990f5e35b7fcc4a7f96dfb3d910c83
Nasccped <pedro.main.tec@gmail.com>
2025-11-07 22:25:16 -0300

    chore: add default config generator

--- lua/rustheme/config.lua
+++ lua/rustheme/config.lua
@@ -1,25 +1,39 @@
+local vimenv = require("rustheme.lib.vimenv")
 local M = {}
 
 ---@type config.RusthemeConfig
 local default_config = {
+    default_variant = {
+        dark = "core-dark",
+        light = "core-light",
+    },
     globals = {
-        bolds = true,
-        italics = true,
+        bolds = {
+            title = true,
+            keyword = true,
+            comment = false,
+            tab_line_sel = true,
+            separators = true,
+            match_paren = true,
+            substitute = true,
+            pmenu_selection = true
+        },
+        italics = {
+            title = false,
+            keyword = true,
+            comment = true,
+            tab_line_sel = true,
+            separators = false,
+            match_paren = false,
+            substitute = false,
+            pmenu_selection = false
+        },
         transparents = {
             background = false,
             float_background = false
-        },
-        invert_title = false,
-        termguicolors = true
+        }
     },
-    mapping_overrides = {
-        everything = {},
-        ["core-dark"] = {},
-        ["core-light"] = {},
-        ["mangrove-dark"] = {},
-        ["mangrove-light"] = {}
-    },
-    group_overrides = {}
+    bg = vimenv.get_background()
 }
 
 --- Returns the default rustheme configuration.
