cb53b2ac96b093faed69a5f24f955f7ea777819d
Nasccped <pedro.main.tec@gmail.com>
2025-11-07 22:24:23 -0300

    chore: add core-dark variant color palette

---
+++ lua/rustheme/palettes/core-dark.lua
@@ -0,0 +1,37 @@
+---@type palette.RusthemePalette
+return {
+    -- editor related stuff (background, foreground, selections, and
+    -- editor's highlights)
+    editor_bg = "#0a0f1a",
+    editor_bg_darker = "#05070c",
+    editor_bg_highlight = "#151929",
+    editor_bg_highlight2 = "#32344c",
+    editor_selection = "#26324c",
+    editor_text = "#e8e8e8",
+    editor_weak_text = "#c1c1c1",
+    editor_contrast = "#ea8666",
+
+    -- ui related stuff (git colors, diag, messages, ...)
+    ui_red = "#dc4141",
+    ui_green = "#57d052",
+    ui_yellow = "#f5c34e",
+    ui_blue = "#4380ef",
+    ui_magenta = "#ec4af8",
+    ui_cyan = "#00c5d1",
+    ui_white = "#eaeaea",
+
+    -- syntax related stuff
+    syntax_orange = "#fb784c",
+    syntax_orange2 = "#fc8c67",
+    syntax_blue = "#82c1ff",
+    syntax_green = "#93d070",
+    syntax_green2 = "#7fed81",
+    syntax_yellow = "#f6cb6f",
+    syntax_yellow2 = "#ffe78b",
+
+    -- color attached to specific tokens (can change HUE over
+    -- palette)
+    syntax_annotation = "#8fefe6",
+    syntax_comment = "#738097",
+    syntax_high_magenta = "#eca1ff",
+}
