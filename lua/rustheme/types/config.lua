5aca0037790fc0f95a5dff3f8702926b14013d89
Nasccped <pedro.main.tec@gmail.com>
2025-11-07 22:19:48 -0300

    chore: add new config fields/remove unused ones

--- lua/rustheme/types/config.lua
+++ lua/rustheme/types/config.lua
@@ -1,20 +1,32 @@
 ---@class config.RusthemeConfig
 ---@field default_variant? config.DefaultVariant
----@field globals? config.GlobalsFields
----@field _current_bg? "light"|"dark" The current background being used.
----@field _variant? config.Variant The theme variant being used.
+---@field globals? config.GlobalFields
+---@field bg? "light"|"dark" The current background being used.
 
 ---@class config.DefaultVariant Maps the rustheme variant based on a background mode.
----@field light? config.Variant Default variant for light background.
----@field dark? config.Variant Default variant for dark background.
+---@field light? config.LightVariant Default variant for light background.
+---@field dark? config.DarkVariant Default variant for dark background.
 
----@alias config.Variant "core-dark"|"core-light"|"mangrove-dark"|"mangrove-light" Alias for variant option.
+---@alias config.AnyVariant config.LightVariant|config.DarkVariant Alias for variant option.
+---@alias config.LightVariant "core-light"|"mangrove-light" Alias for light variants.
+---@alias config.DarkVariant "core-dark"|"mangrove-dark" Alias for dark variants.
 
----@class config.GlobalsFields Config fields that affect all theme variants.
+---@class config.GlobalFields Config fields that affect all theme variants.
+---@field transparents? config.Transparents
 ---@field bolds? config.TokenTrigger
 ---@field italics? config.TokenTrigger
+---@field invert_title? boolean Invert title colors (bg turns into fg and vice-versa).
+
+---@class config.Transparents Config for transparent bgs.
+---@field background? boolean
+---@field float_background? boolean
 
 ---@class config.TokenTrigger Maps a neovim token to a boolean. Should be used by the `bolds` and `italics` fields.
 ---@field title? boolean
 ---@field keyword? boolean
 ---@field comment? boolean
+---@field separators? boolean Window separators.
+---@field tab_line_sel? boolean Current selected tab line.
+---@field match_paren? boolean Matching parents () / [] / {}.
+---@field substitute? boolean Substitute text when running `:s/[OLD]/[NEW]/[OPTIONS]`.
+---@field pmenu_selection? boolean Current Pop-up menu selection.
