59c235791707b190bedd7b1c5d8b878cd682330f
Nasccped <pedro.main.tec@gmail.com>
2025-11-10 13:35:02 -0300

    feat: add core-light palette

---
+++ lua/rustheme/palettes/core-light.lua
@@ -0,0 +1,38 @@
+---@type palette.RusthemePalette
+return {
+    -- editor related stuff (background, foreground, selections, and
+    -- editor's highlights)
+    editor_bg = "#e9eef5",
+    editor_bg_darker = "#d3deec",
+    editor_bg_highlight = "#dde2f2",
+    editor_bg_highlight2 = "#b9cded",
+    editor_selection = "#a1d2ff",
+    editor_text = "#6b7fa8",
+    editor_weak_text = "#7c8eb3",
+    editor_contrast = "#ff6836",
+
+    -- ui related stuff (git colors, diag, messages, ...)
+    ui_black = "#252525",
+    ui_red = "#ff5151",
+    ui_green = "#6aff64",
+    ui_yellow = "#ffd76f",
+    ui_blue = "#4c9bff",
+    ui_magenta = "#ff72f5",
+    ui_cyan = "#82f1ff",
+    ui_white = "#adadad",
+
+    -- syntax related stuff
+    syntax_orange = "#ff6f20",
+    syntax_orange2 = "#ff5b1f",
+    syntax_blue = "#3ea3ff",
+    syntax_green = "#588c3d",
+    syntax_green2 = "#4da652",
+    syntax_yellow = "#c2a24a",
+    syntax_yellow2 = "#f6a41e",
+
+    -- color attached to specific tokens (can change HUE over
+    -- palette)
+    syntax_annotation = "#008bec",
+    syntax_comment = "#9eafc7",
+    syntax_high_magenta = "#b265c8",
+}
