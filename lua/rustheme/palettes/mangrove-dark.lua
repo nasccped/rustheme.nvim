d0439c96b01e93e84a7ea8584e7913a22c66e9f7
Nasccped <pedro.main.tec@gmail.com>
2025-11-10 14:43:57 -0300

    chore: add two mangrove variants (dark + light)

---
+++ lua/rustheme/palettes/mangrove-dark.lua
@@ -0,0 +1,38 @@
+---@type palette.RusthemePalette
+return {
+    -- editor related stuff (background, foreground, selections, and
+    -- editor's highlights)
+    editor_bg = "#1c261b",
+    editor_bg_darker = "#131a12",
+    editor_bg_highlight = "#2c3524",
+    editor_bg_highlight2 = "#3d412d",
+    editor_selection = "#324a3a",
+    editor_text = "#d0e4dc",
+    editor_weak_text = "#899b93",
+    editor_contrast = "#dc8d74",
+
+    -- ui related stuff (git colors, diag, messages, ...)
+    ui_black = "#0d130c",
+    ui_red = "#b14e4e",
+    ui_green = "#57d052",
+    ui_yellow = "#cda853",
+    ui_blue = "#4875c5",
+    ui_magenta = "#be65c5",
+    ui_cyan = "#6db6bc",
+    ui_white = "#b5c1bc",
+
+    -- syntax related stuff
+    syntax_orange = "#df6600",
+    syntax_orange2 = "#ff9169",
+    syntax_blue = "#81aed8",
+    syntax_green = "#b6d682",
+    syntax_green2 = "#7ff57f",
+    syntax_yellow = "#dea94d",
+    syntax_yellow2 = "#ffda74",
+
+    -- color attached to specific tokens (can change HUE over
+    -- palette)
+    syntax_annotation = "#6ef3c9",
+    syntax_comment = "#62715c",
+    syntax_high_magenta = "#eca1ff",
+}
---
+++ lua/rustheme/palettes/mangrove-light.lua
@@ -0,0 +1,38 @@
+---@type palette.RusthemePalette
+return {
+    -- editor related stuff (background, foreground, selections, and
+    -- editor's highlights)
+    editor_bg = "#c3d4b6",
+    editor_bg_darker = "#abbb9e",
+    editor_bg_highlight = "#acc993",
+    editor_bg_highlight2 = "#a7b593",
+    editor_selection = "#93acae",
+    editor_text = "#3c5e52",
+    editor_weak_text = "#5c736b",
+    editor_contrast = "#f36300",
+
+    -- ui related stuff (git colors, diag, messages, ...)
+    ui_black = "#0d130c",
+    ui_red = "#bc2e2e",
+    ui_green = "#35af30",
+    ui_yellow = "#d99c11",
+    ui_blue = "#355ea6",
+    ui_magenta = "#98519d",
+    ui_cyan = "#539ca2",
+    ui_white = "#83978f",
+
+    -- syntax related stuff
+    syntax_orange = "#c86d2c",
+    syntax_orange2 = "#ff6918",
+    syntax_blue = "#3c5f81",
+    syntax_green = "#67910a",
+    syntax_green2 = "#599659",
+    syntax_yellow = "#b48342",
+    syntax_yellow2 = "#c4a400",
+
+    -- color attached to specific tokens (can change HUE over
+    -- palette)
+    syntax_annotation = "#346cd2",
+    syntax_comment = "#889b6f",
+    syntax_high_magenta = "#574b5b",
+}
